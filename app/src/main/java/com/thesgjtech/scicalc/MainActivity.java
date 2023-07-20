bf9c6ac63f65bf4b49aaeb5b21acd98bfe13dde3
Shri Ganesh Jha <60005430+TheSGJ@users.noreply.github.com>
2023-07-21 03:41:01 +0530

    Update MainActivity.java

--- app/src/main/java/com/thesgjtech/scicalc/MainActivity.java
+++ app/src/main/java/com/thesgjtech/scicalc/MainActivity.java
@@ -1,23 +1,16 @@
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+package com.thesgjtech.scicalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-   CalendarView calendar;
-   TextView dateView;
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
-      calendar = findViewById(R.id.calender);
-      dateView = findViewById(R.id.dateView);
-      calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-         @Override
-         public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-            String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
-            dateView.setText(Date);
-         }
-      });
-   }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
