919f3528ea0140a88f37a5e4cb2c2ac72695170e
Shri Ganesh Jha <realsgj@gmail.com>
2023-07-21 10:34:22 +0000

    revert

--- app/src/main/java/com/thesgjtech/scicalc/MainActivity.java
+++ app/src/main/java/com/thesgjtech/scicalc/MainActivity.java
@@ -1,33 +1,17 @@
 package com.thesgjtech.scicalc;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    calendar = findViewById(R.id.calender);
-    dateView = findViewById(R.id.dateView);
-    calendar.setOnDateChangeListener(
-      new CalendarView.OnDateChangeListener() {
-        @Override
-        public void onSelectedDayChange(
-          @NonNull CalendarView view,
-          int year,
-          int month,
-          int dayOfMonth
-        ) {
-          String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
-          dateView.setText(Date);
-        }
-      }
-    );
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
